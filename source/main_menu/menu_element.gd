6fa9c9df3052d594bcde8c8caf4ded5fd154b375
Oz Fox <54425262+OzKitsune@users.noreply.github.com>
2024-07-14 11:32:52 +1000

    add menu_element.gd

---
+++ source/main_menu/menu_element.gd
@@ -0,0 +1,24 @@
+class_name MenuElement
+extends Control
+
+
+signal return_was_pressed(control: Control)
+
+
+@export var return_button: Button
+@export var focus_element: Control
+
+
+func _ready():
+	if return_button != null:
+		return_button.pressed.connect(return_to_menu)
+
+
+func show_and_focus() -> void:
+	show()
+	if focus_element != null:
+		focus_element.grab_focus()
+
+
+func return_to_menu() -> void:
+	return_was_pressed.emit(self)
