f16e6e6f5d340fad21dc170207143a1541008312
Gleb Lisitsky <54425262+OzKitsune@users.noreply.github.com>
2024-12-25 13:39:47 +1000

    65

--- scenes/frame_player/music_player.gd
+++ scenes/frame_player/music_player.gd
@@ -12,50 +12,50 @@ var decrease_time: float = 0.0
 var current_linear: float = 1.0
 
 
-func _process(delta):
-	if increase_time > 0:
-		increase_time -= delta
-		volume_db = linear_to_db(1.0 - increase_time)
-	
-	if decrease_time > 0:
-		decrease_time -= delta
-		if decrease_time <= 0:
-			decrease_time = 0
-			stop()
-		var db = linear_to_db(decrease_time)
-		if db <= volume_db:
-			volume_db = db
+#func _process(delta):
+	#if increase_time > 0:
+		#increase_time -= delta
+		#volume_db = linear_to_db(1.0 - increase_time)
+	#
+	#if decrease_time > 0:
+		#decrease_time -= delta
+		#if decrease_time <= 0:
+			#decrease_time = 0
+			#stop()
+		#var db = linear_to_db(decrease_time)
+		#if db <= volume_db:
+			#volume_db = db
 
 
 func play_music(music: RSEMusic) -> void:
 	if music == current_music:
 		if not playing:
-			_play()
+			play()
 		return
 	
 	current_music = music
 	#print(music.name)
 	
 	if music.name == "None":
-		_stop()
-		#stream = null
+		stop()
+		stream = null
 		return
 	
 	if music.path_to_file == null or music.path_to_file == "":
-		_stop()
+		stop()
 		return
 	
 	stream = load(music.path_to_file)
-	_play()
-
-
-func _play() -> void:
-	increase_time = transitition_seconds
-	decrease_time = 0
-	volume_db = min_db
 	play()
 
-
-func _stop() -> void:
-	increase_time = 0
-	decrease_time = transitition_seconds
+#
+#func _play() -> void:
+	#increase_time = transitition_seconds
+	#decrease_time = 0
+	#volume_db = min_db
+	#play()
+#
+#
+#func _stop() -> void:
+	#increase_time = 0
+	#decrease_time = transitition_seconds
