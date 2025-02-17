01286f318da7a63987d896dd55d5099881313a8b
Gleb Lisitsky <54425262+OzKitsune@users.noreply.github.com>
2025-02-17 11:03:37 +1000

    update save_container.gd

--- scenes/save_manager/save_container.gd
+++ scenes/save_manager/save_container.gd
@@ -17,11 +17,9 @@ func set_save(save: Save) -> void:
 	_save = save
 	if save.image != null:
 		image.texture = ImageTexture.create_from_image(save.image)
-	var datetime = "%s.%02d.%02d %02d:%02d:%02d" % [save.datetime["year"], save.datetime["month"], save.datetime["day"], save.datetime["hour"], save.datetime["minute"], save.datetime["second"]]
-	label.text = ""
-	if save.auto_save:
-		label.text = "[color=INDIAN_RED][%s][/color] " % tr("АВТО")
-	label.text += "%s\n%s" % [tr(save.episode_name), datetime]
+	_update_text()
+	
+	Settings.locale_was_changed.connect(_update_text)
 
 
 func _on_load_pressed():
@@ -32,3 +30,11 @@ func _on_delete_pressed():
 	if _save == null:
 		return
 	delete_save.emit(self)
+
+
+func _update_text() -> void:
+	label.text = ""
+	if _save.auto_save:
+		label.text = "[color=INDIAN_RED][%s][/color] " % tr("АВТО")
+	var datetime = "%s.%02d.%02d %02d:%02d:%02d" % [_save.datetime["year"], _save.datetime["month"], _save.datetime["day"], _save.datetime["hour"], _save.datetime["minute"], _save.datetime["second"]]
+	label.text += "%s\n%s" % [tr(_save.episode_name), datetime]
